d66ee542449014d980858310f7b3b1f9981d301f
ALLEN <ca5219@att.com>
2021-06-25 16:51:56 -0400

    Initial Commit

---
+++ server/routes/movieAPI.js
@@ -0,0 +1,8 @@
+const express = require('express')
+var router = express.Router();
+
+router.get("/", (req, res, next) => {
+    res.send('API is functioning properly');
+});
+
+module.exports = router;
